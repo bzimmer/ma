fbf5f35dceea522f184095bc3bf6669b83d3666f
Brian Zimmer <bzimmer@ziclix.com>
2025-12-22 18:35:56 +0100

    log decoding errors

--- ls.go
+++ ls.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 
 	"github.com/bzimmer/smugmug"
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
 
 func image(c *cli.Context) error {
 	mg := runtime(c).Smugmug()
+	expansions := smugmug.WithExpansions("ImageAlbum", "ImageMetadata", "ImageSizeDetails")
 	for i := range c.NArg() {
 		id := c.Args().Get(i)
 		if !imageRE.MatchString(id) {
 			id = fmt.Sprintf("%s-0", id)
 		}
-		image, err := mg.Image.Image(c.Context, id, smugmug.WithExpansions("ImageAlbum", "ImageMetadata"))
+		log.Debug().Str("imageKey", id).Msg("retrieving image")
+		image, err := mg.Image.Image(c.Context, id, expansions)
 		if err != nil {
+			log.Error().Err(err).Str("imageKey", id).Msg("error retrieving image")
 			return err
 		}
 		f := imageIterFunc(c, image.Album, "ls")
@@ -28,10 +32,12 @@ func image(c *cli.Context) error {
 
 func album(c *cli.Context) error {
 	mg := runtime(c).Smugmug()
-	f := albumIterFunc(c, "ls", smugmug.WithExpansions("ImageAlbum", "ImageMetadata"))
+	f := albumIterFunc(c, "ls", smugmug.WithExpansions("ImageAlbum", "ImageMetadata", "ImageSizeDetails"))
 	for _, id := range c.Args().Slice() {
+		log.Debug().Str("albumKey", id).Msg("retrieving album")
 		album, err := mg.Album.Album(c.Context, id)
 		if err != nil {
+			log.Error().Err(err).Str("albumKey", id).Msg("error retrieving album")
 			return err
 		}
 		if _, err = f(album); err != nil {
@@ -54,7 +60,9 @@ func node(c *cli.Context) error {
 	depth := c.Int("depth")
 	f := nodeIterFunc(c, c.Bool("recurse"), "ls")
 	for i := range nodeIDs {
+		log.Debug().Str("nodeID", nodeIDs[i]).Msg("walking node")
 		if err := mg.Node.WalkN(c.Context, nodeIDs[i], f, depth, smugmug.WithExpansions("Album", "ParentNode")); err != nil {
+			log.Error().Err(err).Str("nodeID", nodeIDs[i]).Msg("error retrieving node")
 			return err
 		}
 	}
